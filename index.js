d90877c28391dd3958da3a9b87b21a2c1b68ac84
sos171805jpr <b-cube@hotmail.es>
2018-03-04 17:59:56 +0000

    F04

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+var express = require("express");
 var coll = require("cool-ascii-faces");
 
+var app = express();
+
+app.get("/hello",(req,res)=>{
+    res.send("Hello World");
+});
+
+app.listen(process.env.PORT);
 
 console.log(coll());
