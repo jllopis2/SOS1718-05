26b30b40c8f0497612b3e7f2870b9a68c0de8358
sos171805jpr <b-cube@hotmail.es>
2018-03-04 20:54:59 +0000

    Feddback F04

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+var express = require("express");
+var app = express();
+
+
+app.get("/hello",(req,res)=>{
+    res.send("Hello World");
+});
+
+app.listen(process.env.PORT);
